b44765fc2f2230deaddc3436cac63944ea84de2d
ChandanaMohan20 <83974650+ChandanaMohan20@users.noreply.github.com>
2024-06-22 02:11:38 +0530

    Create sol.py

---
+++ 0268-missing-number/sol.py
@@ -0,0 +1,15 @@
+ maxval = nums[0]
+        
+        for i in range(0,len(nums)):
+            if maxval> nums[i]:
+                maxval = nums[i]
+                
+        nums.sort()
+                        
+                
+                        
+        for i in range(0,len(nums)):
+            if nums[i]!=i:
+                return i
+        return len(nums)
+            
