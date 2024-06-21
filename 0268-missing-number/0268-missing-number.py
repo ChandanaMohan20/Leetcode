2e9407d541dc82a75098730cbe0cdf9874f5d002
ChandanaMohan20 <83974650+ChandanaMohan20@users.noreply.github.com>
2024-06-22 02:43:46 +0530

    Time: 95 ms (64.26%), Space: 12.9 MB (35.55%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,20 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        maxval = nums[0]
         
-        for i in range(0,len(nums)):
-            if maxval> nums[i]:
-                maxval = nums[i]
-                
-        nums.sort()
-                        
-                
-                        
-        for i in range(0,len(nums)):
-            if nums[i]!=i:
-                return i
-        return len(nums)
+        res = 0
+        
+        for i in range(0,len(nums)+1):
+            res = res ^ i
             
+        for num in nums:
+            res = num ^ res
             
-            
+        return res
