839fcb0072e88622b9a68e6ef3547637742c9550
Chandana Mohan <83974650+ChandanaMohan20@users.noreply.github.com>
2024-11-27 13:42:53 -0600

    Time: 375 ms (69.66%), Space: 17.3 MB (7.51%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -9,23 +9,26 @@ class Solution:
             for j in range(i+1,n-2):
                 if j>i+1 and nums[j]==nums[j-1]:
                     continue
-                
                 l = j+1
                 r = n-1
                 
-                while l<r :
-                    numsum=nums[i]+nums[j]+nums[l]+nums[r]
-                    if numsum <target:
+                while(l<r):
+                    numsum = nums[i]+nums[j]+nums[l]+nums[r]
+                    if numsum < target:
                         l = l+1
-                    elif numsum>target:
+                    elif numsum > target:
                         r = r-1
                     else:
                         res.append([nums[i],nums[j],nums[l],nums[r]])
                         
                         while l<r and nums[l]==nums[l+1]:
-                            l=l+1
+                            l = l+1
                         while l<r and nums[r]==nums[r-1]:
-                            r=r-1
+                            r = r-1
                         l = l+1
-                        r=r-1
+                        r = r-1
+                    
         return res
+                        
+                
+            
