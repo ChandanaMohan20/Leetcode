2011b64c518eb7b036c94ffd7f366650cbb7108d
Chandana Mohan <83974650+ChandanaMohan20@users.noreply.github.com>
2024-11-27 19:56:37 -0600

    Time: 181 ms (17.79%), Space: 78.1 MB (31.25%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,27 +1,26 @@
 class ListNode:
-    def __init__(self, key, val):
+    def __init__(self,key,val):
         self.key = key
         self.val = val
-        self.prev = None
         self.next = None
-        
-        
+        self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.node_map = {}
-        
         self.head = ListNode(-1,-1)
         self.tail = ListNode(-1,-1)
         
         self.head.next = self.tail
         self.tail.prev = self.head
+        
 
     def get(self, key: int) -> int:
         if key not in self.node_map:
             return -1
+        
         node = self.node_map[key]
         self.remove(node)
         self.add(node)
@@ -29,35 +28,33 @@ class LRUCache:
 
     def put(self, key: int, value: int) -> None:
         if key in self.node_map:
-            old_value = self.node_map[key]
-            self.remove(old_value)
-            
+            node_to_remove = self.node_map[key]
+            self.remove(node_to_remove)
+        #adding key to cache
         node = ListNode(key,value)
+        
         self.node_map[key] = node
         self.add(node)
         
-        if len(self.node_map)>self.capacity:
-            node_to_remove = self.head.next
-            self.remove(node_to_remove)
-            del self.node_map[node_to_remove.key]
+        if len(self.node_map)> self.capacity :
+            node_to_delete = self.head.next
+            self.remove(node_to_delete)
+            
+            del self.node_map[node_to_delete.key]
             
             
         
     def add(self,node):
-       
-        prev_node = self.tail.prev
-        prev_node.next = node
+        prev_node= self.tail.prev
         
-        node.next = self.tail
         node.prev = prev_node
+        prev_node.next = node
+        node.next = self.tail
         self.tail.prev = node
         
     def remove(self,node):
-        
-        node.prev.next = node.next
         node.next.prev = node.prev
-        
-        
+        node.prev.next = node.next
 
 
 # Your LRUCache object will be instantiated and called as such:
