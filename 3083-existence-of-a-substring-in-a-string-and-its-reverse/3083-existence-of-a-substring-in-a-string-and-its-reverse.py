658da52ac404085e00926f85479e67aa4b0cfa94
Chandana Mohan <83974650+ChandanaMohan20@users.noreply.github.com>
2024-12-01 12:06:32 -0600

    Time: 0 ms (100.00%), Space: 17.5 MB (7.28%) - LeetHub

--- 3083-existence-of-a-substring-in-a-string-and-its-reverse/3083-existence-of-a-substring-in-a-string-and-its-reverse.py
+++ 3083-existence-of-a-substring-in-a-string-and-its-reverse/3083-existence-of-a-substring-in-a-string-and-its-reverse.py
@@ -1,9 +1,10 @@
 class Solution:
     def isSubstringPresent(self, s: str) -> bool:
+        revs = s[::-1]
         for i in range(len(s)-1):
             
             subs =  s[i:i+2]
-            revs = s[::-1]
+            
             if subs in revs :
                 return True
             subs=""
