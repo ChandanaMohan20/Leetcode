cb2ecf60b8ca242500cf7c03441ddd1cf615619f
Chandana Mohan <83974650+ChandanaMohan20@users.noreply.github.com>
2024-12-14 21:17:50 -0600

    Time: 27 ms (30.97%), Space: 17.4 MB (14.77%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        hm1 = {}
+        hm2 = {}
+        
+        for i in ransomNote:
+            if i not in hm1:
+                hm1[i] = 1
+            else:
+                hm1[i]+=1
+        
+        for i in magazine:
+            if i not in hm2:
+                hm2[i] = 1
+            else:
+                hm2[i]+=1
+                
+        for i,j in hm1.items():
+            if i not in hm2.keys() or j>hm2[i]:
+                return False
+        return True
+            
+                
+        
+                
+                
+                
