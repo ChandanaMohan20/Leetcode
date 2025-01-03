062455f9ef63e7f0e41695c4912050430994bc16
Chandana Mohan <83974650+ChandanaMohan20@users.noreply.github.com>
2025-01-02 23:22:08 -0600

    Time: 42 ms, Memory: 32.5 MB - LeetHub

--- 0715-67-2270-number-of-ways-to-split-array/0715-67-2270-number-of-ways-to-split-array.py
+++ 0715-67-2270-number-of-ways-to-split-array/0715-67-2270-number-of-ways-to-split-array.py
@@ -3,11 +3,11 @@ class Solution:
         total_sum = sum(nums)
         left_sum = 0
         count = 0
-
+        
         for i in range(len(nums)-1):
             left_sum +=nums[i]
             right_sum = total_sum - left_sum
-
+            
             if left_sum >= right_sum:
                 count+=1
         return count
